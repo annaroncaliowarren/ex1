21ced1d7c4aa5c83ef489c35d12c629eb8da49ff
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-06-30 14:03:04 -0300

    change desafio.dart

--- desafio.dart
+++ desafio.dart
@@ -2,21 +2,27 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  List<int> dice = [1, 2, 3, 4, 5, 6];
-
-  Random round = Random();
-
-  int result = dice[round.nextInt(dice.length)];
+  //List<int> dice = [1, 2, 3, 4, 5, 6];
 
   print("Qual é o seu nome?");
 
   String name = stdin.readLineSync()!;
 
-  print("$name, escolha um número de 1 a 6 e veja se você acerta o número do dado");
+  print("$name, quantos lados você quer que o seu dado tenha?");
+
+  int sizeDice = int.parse(stdin.readLineSync()!);
+
+  List<int> dice = new List<int>.generate(sizeDice, (i) => i + 1);
+
+  Random round = Random();
+
+  int result = dice[round.nextInt(dice.length)];
+
+  print("$name, escolha um número e veja se você acerta o número do dado");
 
   int number = int.parse(stdin.readLineSync()!);
 
-  if (number == result){
+  if (number == result) {
     print("Parabéns, você acertou!");
   } else {
     print("Tente novamente! O dado caiu no número $result.");
