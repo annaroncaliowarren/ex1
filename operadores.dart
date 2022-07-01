73d278b8d76f8f72564dcd2b8a7ee06be2cc4565
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-01 09:16:22 -0300

    add operadores.dart

---
+++ operadores.dart
@@ -0,0 +1,34 @@
+void main() {
+
+  double nota1 = 10;
+  double nota2 = 9.5;
+
+  // soma
+  print(nota1 + nota2);
+
+  // subtrair
+  print(nota1 - nota2);
+
+  // multiplicar
+  print(nota1 * nota2);
+
+  // divisõa
+  print(nota1 / nota2);
+  print(nota1 ~/ nota2); // retorna apenas a parte inteira da divisão
+  print(nota1 % nota2); // resto da divisão
+
+  // inversão
+  // print(nota1 = -nota2);
+
+  // maior / maior ou igual
+  print(nota1 > nota2);
+  print(nota1 >= nota2);
+
+  // menor / menor ou igual
+  print(nota1 < nota2);
+  print(nota1 <= nota2);
+
+  // igualdade / diferente
+  print(nota1 == nota2);
+  print(nota1 != nota2);
+}
--- variaveis.dart
+++ variaveis.dart
@@ -4,6 +4,9 @@ void main() {
   num numeroNormal = 1;
   int numeroInteiro = 2;
   double numeroFlutuante = 1.65;
+  numeroFlutuante.round();
+  numeroFlutuante.floor();
+  numeroInteiro.toDouble();
 
   //tipo string
   String stringNormal = 'Cadeira';
@@ -11,7 +14,7 @@ void main() {
   Warren
   Flutter
   C# ''';
-
+    
   //tipo booleans
   bool verdadeiro = true;
   bool falso = false;
@@ -20,11 +23,17 @@ void main() {
   List<int> listaNumeros = [1, 2, 3, 2, 3];
   List<String> listaString = ['Proway', 'Warren'];
   List<dynamic> listaDiversa = ['Flutter', 26];
-
-  print(listaNumeros.where((numero) => numero < 2));
-  listaNumeros.insert(2, 5);
-  print(listaNumeros);
-
+  
+  listaDiversa.contains('Flutter'); // true or false
+  listaDiversa.addAll(['Dart', 26]);
+  listaDiversa.add(30);
+  listaDiversa.removeAt(1); // remove elemento nesse index
+  listaDiversa.shuffle();
+  print(listaNumeros.isEmpty);
+  print(listaNumeros.where((numero) => numero < 2)); // volta o elemento se for true, se for false volta vazio
+  print(listaNumeros.any((numero) => numero < 2)); // true or false
+  listaNumeros.insert(2, 5); // insere elemento 5 no index 2 (index, elemento)
+  
   //tipo sets
   Set<int> setNumero = {1, 2, 3};
   Set<dynamic> setDiverso = {'Proway', 1};
@@ -34,6 +43,11 @@ void main() {
     'Proway' : '27/06',
     'Warren' : '06/09'
   };
+
+  print(mapExemplo.keys);
+  mapExemplo.addAll({'Idade' : 26});
+  mapExemplo.addEntries(<String, dynamic>{'Aluno' : 'Anna'}.entries);
+  print(mapExemplo);
 
   final mapExemplo2 = <String, dynamic>{};
     mapExemplo2.addAll({'Proway' : '27/06'});
