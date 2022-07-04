9bf701331aa86e72df2bab6abf6750c2ad54802d
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-04 10:58:12 -0300

    [change] change desafio.dart

--- desafio.dart
+++ desafio.dart
@@ -2,31 +2,50 @@ import 'dart:io' as input;
 import 'dart:math' as math;
 
 void main() {
+  print('Crie o dado para o seu jogo! (Digite sair se quiser abandonar)');
+  input.stdout.write("Primeiro, qual é o seu nome? ");
+
+  String inputName = input.stdin.readLineSync()!;
+
+  input.stdout
+    .write("$inputName, quantos lados você quer que o seu dado tenha? ");
+
+  int inputSizeDice = int.parse(input.stdin.readLineSync()!);
+
+  print('Dado criado!');
   
-  print("Qual é o seu nome?");
+  while (true) {
+    input.stdout.write('Digite jogar quando quiser que o dado role: ');
+    String inputPlay = input.stdin.readLineSync()!.toLowerCase();
 
-  String name = input.stdin.readLineSync()!;
-
-
-  print("$name, quantos lados você quer que o seu dado tenha?");
-
-  int sizeDice = int.parse(input.stdin.readLineSync()!);
-
-  List<int> dice = new List<int>.generate(sizeDice, (i) => i + 1);
-
-  math.Random round = math.Random();
-
-  int result = dice[round.nextInt(dice.length)];
-
-
-  print("$name, escolha um número até $sizeDice e veja se você consegue prever o futuro");
-
-  int number = int.parse(input.stdin.readLineSync()!);
-
-  if (number == result) {
-    print("Parabéns, vidente nato!");
-  } else {
-    print("Tente novamente! O dado caiu no número $result.");
+    if (inputPlay == 'jogar') {
+      print('Resultado do dado: ${play(inputSizeDice)}');
+      continue;
+    } else if (inputPlay == 'sair') {
+      print('Até depois!');
+      break;
+    } else if (inputPlay != 'jogar') {
+      print('Se quiser que o dado role, digite apenas JOGAR.');
+      continue;
+    }
   }
 
+  // input.stdout.write(
+  //     "$inputName, escolha um número até $inputSizeDice e veja se você consegue prever o futuro: ");
+
+  // int inputNumber = int.parse(input.stdin.readLineSync()!);
+
+  // if (inputNumber == result) {
+  //   print("Parabéns, vidente nato!");
+  // } else {
+  //   print("Não foi dessa vez... O dado caiu no número $result.");
+  // }
+}
+
+play(inputNumber) {
+  List<int> dice = new List<int>.generate(inputNumber, (i) => i + 1);
+
+  int result = dice[math.Random().nextInt(dice.length)];
+
+  return result;
 }
